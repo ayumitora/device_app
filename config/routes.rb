e8720e5d717e8b244b9e44f22003235028b94901
ayumi_arai <0araiayumi@gmail.com>
2019-09-06 10:08:55 +0900

    レターオープナーまわり細かい修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'blogs#index'
   resources :blogs
   if Rails.env.development?
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
